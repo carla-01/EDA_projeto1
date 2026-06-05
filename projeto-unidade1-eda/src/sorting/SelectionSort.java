be0bb2579fdba91316bb3bef220e0e60ff4f3889
bryan <bryanandrade236@gmail.com>
2026-06-05 16:17:54 -0300

    att selectionsort
    teste selectionsort

--- projeto-unidade1-eda/src/sorting/SelectionSort.java
+++ projeto-unidade1-eda/src/sorting/SelectionSort.java
@@ -2,4 +2,23 @@ package sorting;
 
 public class SelectionSort {
 
+// ---------------------------------------------------------------
+/* Versão do SLIDE:
+ *   - Encontra o menor elemento e troca com a posição i
+ *   - Θ(n²) comparações; O(n) trocas
+ *   - A versão com swap direto NÃO é estável
+ *     Ex.: [2a, 2b, 1] → swap(2a,1) → [1, 2b, 2a]  (2a e 2b invertidos) */
+// ---------------------------------------------------------------
+    public static <T extends Comparable<T>> void sort(T[] A) {
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (A[j].compareTo(A[min]) < 0) {
+                    min = j;
+                }
+            }
+            swap(A, i, min);
+        }
+}
 }
--- projeto-unidade1-eda/src/sorting/SelectionSortEstavel.java
+++ projeto-unidade1-eda/src/sorting/SelectionSortEstavel.java
@@ -3,3 +3,35 @@ package sorting;
 public class SelectionSortEstavel {
 
 }
+/* ---------------------------------------------------------------
+    * Versão ESTÁVEL
+    * Encontra o mínimo, desloca os elementos entre i e min
+    * uma posição à direita, e insere o mínimo em i.
+// --------------------------------------------------------------- */
+    public static <T extends Comparable<T>> void sortEstavel(T[] A) {
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (A[j].compareTo(A[min]) < 0) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                T minElem = A[min];
+                // desloca elementos de i até min-1 uma posição à direita
+                for (int k = min; k > i; k--) {
+                    A[k] = A[k - 1];
+                }
+                A[i] = minElem;
+            }
+        }
+    }
+ 
+    // ---------------------------------------------------------------
+    private static <T> void swap(T[] A, int i, int j) {
+        T temp = A[i];
+        A[i]   = A[j];
+        A[j]   = temp;
+    }
+}
