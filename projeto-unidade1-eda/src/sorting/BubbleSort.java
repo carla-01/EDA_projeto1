1d8aaca582b635447ea175ac2e48a2292e3d9918
bryan <bryanandrade236@gmail.com>
2026-06-05 15:48:39 -0300

    att bubblesort
    teste bubblesort

--- projeto-unidade1-eda/src/sorting/BubbleSort.java
+++ projeto-unidade1-eda/src/sorting/BubbleSort.java
@@ -1,4 +1,21 @@
 package sorting;
 
 public class BubbleSort {
+
+/* BUBBLESORT
+ Versão do SLIDE:
+ *   - Percorre o vetor n-1 vezes
+ *   - A cada passagem faz flutuar o maior elemento para o topo
+ *   - Θ(n²) em todos os casos */
+
+    public static <T extends Comparable<T>> void sort(T[] A) {
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (A[j].compareTo(A[j + 1]) > 0) {
+                    swap(A, j, j + 1);
+                }
+            }
+        }
+    }
 }
--- projeto-unidade1-eda/src/sorting/BubbleSortOtimizado.java
+++ projeto-unidade1-eda/src/sorting/BubbleSortOtimizado.java
@@ -2,4 +2,28 @@ package sorting;
 
 public class BubbleSortOtimizado {
 
+ /* Versão OTIMIZADA do BubbleSort:
+ *   - Igual ao slide, mas interrompe se nenhuma troca ocorreu na passagem
+ *   - Melhor caso Ω(n) (vetor já ordenado)
+ *   - Pior caso Θ(n²)
+ */
+    public static <T extends Comparable<T>> void sortOtimizado(T[] A) {
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean trocou = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (A[j].compareTo(A[j + 1]) > 0) {
+                    swap(A, j, j + 1);
+                    trocou = true;
+                }
+            }
+            if (!trocou) break; // vetor já ordenado
+        }
+    }
+
+    private static <T> void swap(T[] A, int i, int j) {
+        T temp = A[i];
+        A[i]   = A[j];
+        A[j]   = temp;
+    }
 }
