9293fb396b4974c095bb85304c51bd53a97d5bd3
Adrielly  (✿◡‿◡) <130372089+carla-01@users.noreply.github.com>
2026-06-07 11:18:48 -0300

    [main] - implement search
    BuscaBinaria (recursiva e iterativa) e BuscaDuasPontas

--- projeto-unidade1-eda/src/search/BuscaBinariaIterativa.java
+++ projeto-unidade1-eda/src/search/BuscaBinariaIterativa.java
@@ -1,4 +1,37 @@
 package search;
 
+import java.util.Comparator;
+
 public class BuscaBinariaIterativa {
+
+	public static <T extends Comparable<? super T>> int busca(T[] A, T chave) {
+		return busca(A, chave, Comparator.naturalOrder());
+	}
+
+	public static <T> int busca(T[] A, T chave, Comparator<? super T> comp) {
+		if (A == null || A.length == 0) return -1;
+		int left = 0;
+		int right = A.length - 1;
+		while (left <= right) {
+			int mid = left + ((right - left) >>> 1);
+			int cmp = comp.compare(A[mid], chave);
+			if (cmp == 0) return mid;
+			if (cmp < 0) left = mid + 1;
+			else right = mid - 1;
+		}
+		return -1;
+	}
+
+	public static int busca(int[] A, int chave) {
+		if (A == null || A.length == 0) return -1;
+		int left = 0, right = A.length - 1;
+		while (left <= right) {
+			int mid = left + ((right - left) >>> 1);
+			if (A[mid] == chave) return mid;
+			if (A[mid] < chave) left = mid + 1;
+			else right = mid - 1;
+		}
+		return -1;
+	}
+
 }
--- projeto-unidade1-eda/src/search/BuscaDuasPontas.java
+++ projeto-unidade1-eda/src/search/BuscaDuasPontas.java
@@ -2,4 +2,30 @@ package search;
 
 public class BuscaDuasPontas {
 
+	public static <T> int busca(T[] A, T chave) {
+		if (A == null || A.length == 0) return -1;
+		int n = A.length;
+		for (int i = 0; i <= (n - 1) / 2; i++) {
+			T left = A[i];
+			if (left == null ? chave == null : left.equals(chave)) return i;
+			int j = n - 1 - i;
+			if (j != i) {
+				T right = A[j];
+				if (right == null ? chave == null : right.equals(chave)) return j;
+			}
+		}
+		return -1;
+	}
+
+	public static int busca(int[] A, int chave) {
+		if (A == null || A.length == 0) return -1;
+		int n = A.length;
+		for (int i = 0; i <= (n - 1) / 2; i++) {
+			if (A[i] == chave) return i;
+			int j = n - 1 - i;
+			if (j != i && A[j] == chave) return j;
+		}
+		return -1;
+	}
+
 }
